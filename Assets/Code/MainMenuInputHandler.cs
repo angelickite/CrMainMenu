496c5731b6719b0d25a28d89b8705813cab5a673
angelickite <angelickite@gmail.com>
2017-02-03 16:52:19 +0100

    Initial commit

---
+++ Assets/Code/MainMenuInputHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuInputHandler : MonoBehaviour {
+    
+    public void OnPlayButton()
+    {
+        Debug.Log("Loading 'Gameplay' Scene...");
+        // Application.LoadLevel("Gameplay");
+    }
+
+    public void OnInstructionsButton()
+    {
+        Debug.Log("Showing Instructions...");
+    }
+
+    public void OnExitButton()
+    {
+        Debug.Log("Exiting Application...");
+        Application.Quit();
+    }
+
+}
